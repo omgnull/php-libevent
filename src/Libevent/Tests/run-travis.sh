b8c07ae4a7f2c6057b88aa7f068b28142ebf5f54
Igor Makarov <doomsmile@gmail.com>
2013-03-18 01:59:09 +0400

    More travis ci try

--- src/Libevent/Tests/run-travis.sh
+++ src/Libevent/Tests/run-travis.sh
@@ -2,7 +2,7 @@
 
 echo "\nstart to buil libevent libs & headers"
 sudo apt-get install libevent-1.4.2 libevent-dev
-sudo pear upgrade
+pear upgrade
 
 echo "\nstart installing libevent extension"
 pecl install -f channel://pecl.php.net/libevent-0.0.5 --with-libevent
